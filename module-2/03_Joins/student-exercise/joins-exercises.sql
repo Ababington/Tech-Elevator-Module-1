ff09a845aa92d4c3da6d225222ffcc5da890358c
August Babington <augustbabington@gmail.com>
2020-10-15 14:22:26 -0400

    current progress

--- module-2/03_Joins/student-exercise/joins-exercises.sql
+++ module-2/03_Joins/student-exercise/joins-exercises.sql
@@ -117,21 +117,77 @@ order by most_rentals desc
 -- 14. The first and last name of the top ten customers ranked by dollars spent 
 -- (#1 should be “KARL SEAL” with 221.55 spent, #10 should be “ANA BRADLEY” with 174.66 spent)
 
+select top 10 customer.first_name, customer.last_name, sum(amount)as dollars_spent from payment 
+join customer on payment.customer_id = customer.customer_id
+group by customer.first_name, customer.last_name
+order by dollars_spent desc
+
 -- 15. The store ID, street address, total number of rentals, total amount of sales (i.e. payments), and average sale of each store.
 -- (NOTE: Keep in mind that an employee may work at multiple stores.)
 -- (Store 1 has 7928 total rentals and Store 2 has 8121 total rentals)
 
+select store.store_id, address.address, count(rental.inventory_id)as most_rentals, sum(amount)as dollars_spent, avg(amount)as dollars_avg from store 
+join address on store.address_id = address.address_id
+join payment on rental.customer_id = payment.rental_id
+join customer on store.store_id = customer.store_id
+join inventory on store.store_id = inventory.store_id
+join rental on inventory.inventory_id = rental.inventory_id
+
+
+
 -- 16. The top ten film titles by number of rentals
 -- (#1 should be “BUCKET BROTHERHOOD” with 34 rentals and #10 should have 31 rentals)
+select top 10 film.title, count(rental.inventory_id) as mostrentals from film
+join inventory on film.film_id = inventory.film_id
+join rental on inventory.inventory_id = rental.inventory_id
+group by title
+order by mostrentals desc
 
 -- 17. The top five film categories by number of rentals 
 -- (#1 should be “Sports” with 1179 rentals and #5 should be “Family” with 1096 rentals)
 
+select top 5 name, count(rental.inventory_id) as mostrentals2 from film
+join film_category as fc on fc.film_id = film.film_id
+join category on category.category_id = fc.category_id
+join inventory on film.film_id = inventory.film_id
+join rental on inventory.inventory_id = rental.inventory_id
+group by category.name 
+order by mostrentals2 desc
+
 -- 18. The top five Action film titles by number of rentals 
 -- (#1 should have 30 rentals and #5 should have 28 rentals)
+
+select top 5 title, count(rental.inventory_id) as mostrentals3 from film
+join film_category as fc on fc.film_id = film.film_id
+join category on category.category_id = fc.category_id
+join inventory on film.film_id = inventory.film_id
+join rental on inventory.inventory_id = rental.inventory_id
+where category.name = 'Action'
+group by title
+order by mostrentals3 desc
+
 
 -- 19. The top 10 actors ranked by number of rentals of films starring that actor 
 -- (#1 should be “GINA DEGENERES” with 753 rentals and #10 should be “SEAN GUINESS” with 599 rentals)
 
+select top 10 actor.first_name,actor.last_name, count(rental.inventory_id) as mostrentals4 from film
+join film_actor on film.film_id = film_actor.film_id
+join actor on film_actor.actor_id = actor.actor_id
+join inventory on film.film_id = inventory.film_id
+join rental on inventory.inventory_id = rental.inventory_id
+group by actor.first_name,actor.last_name
+order by mostrentals4 desc
+
 -- 20. The top 5 “Comedy” actors ranked by number of rentals of films in the “Comedy” category starring that actor 
 -- (#1 should have 87 rentals and #5 should have 72 rentals)
+
+select top 5 actor.first_name,actor.last_name, count(rental.inventory_id) as mostrentals5, title from film
+join film_actor on film.film_id = film_actor.film_id
+join actor on film_actor.actor_id = actor.actor_id
+join film_category as fc on fc.film_id = film.film_id
+join category on category.category_id = fc.category_id
+join inventory on film.film_id = inventory.film_id
+join rental on inventory.inventory_id = rental.inventory_id
+where category.name = 'Comedy'
+group by actor.first_name,actor.last_name
+order by mostrentals5 desc
