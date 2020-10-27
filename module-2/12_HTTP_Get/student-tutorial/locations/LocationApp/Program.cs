0a51c680ae51183d09d5ba766ce8ec7fb7319794
August Babington <augustbabington@gmail.com>
2020-10-27 08:36:26 -0400

    http get tutorial completion

--- module-2/12_HTTP_Get/student-tutorial/locations/LocationApp/Program.cs
+++ module-2/12_HTTP_Get/student-tutorial/locations/LocationApp/Program.cs
@@ -6,12 +6,46 @@ namespace LocationApp
 {
     class Program
     {
+        const string API_URL = "http://localhost:3000/locations";
         static void Main(string[] args)
         {
             Run();
         }
         private static void Run()
         {
+            int menuSelection;
+            PrintGreeting();
+            if (!int.TryParse(Console.ReadLine(), out menuSelection))
+            {
+                Console.WriteLine("Invalid input. Only input a number.");
+            }
+            if (menuSelection == 1)
+            {
+                // list locations
+                {
+                    RestClient client = new RestClient();
+                    RestRequest request = new RestRequest(API_URL);
+                    IRestResponse<List<Location>> response = client.Get<List<Location>>(request);
+                    PrintLocations(response.Data);
+                    int id;
+                    if (!int.TryParse(Console.ReadLine(), out id))
+                    {
+                        Console.WriteLine("Invalid input. Only input a number.");
+                    }
+
+                    if (id > 0 && id <= response.Data.Count)
+                    {
+                        RestRequest requestOne = new RestRequest(API_URL + "/" + id);
+                        IRestResponse<Location> location = client.Get<Location>(requestOne);
+                        PrintLocation(location.Data);
+                    }
+
+                }
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
 
         private static void PrintGreeting()
@@ -49,6 +83,8 @@ namespace LocationApp
             Console.WriteLine("City: " + location.City);
             Console.WriteLine("State: " + location.State);
             Console.WriteLine("Zip: " + location.Zip);
+
+
         }
     }
 }
