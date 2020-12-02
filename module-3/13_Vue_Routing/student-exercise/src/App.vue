adcb862322ace90f50af7569517e20371a3ed264
August Babington <augustbabington@gmail.com>
2020-12-02 08:56:58 -0500

    commit

--- module-3/13_Vue_Routing/student-exercise/src/App.vue
+++ module-3/13_Vue_Routing/student-exercise/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
+    <nav>
+    <router-link v-bind:to="{name: 'Home'}" />
+    <router-link v-bind:to="{name: 'MyBooks'}" />
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
